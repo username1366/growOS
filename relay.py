6e2e1f6146acf62456be17f507ba0db384873eae
alexander.kosenko <alexander.kosenko@loopme.com>
2016-12-03 02:57:45 +0200

    Sat Dec  3 02:57:45 EET 2016

--- relay.py
+++ relay.py
@@ -15,50 +15,33 @@ GPIO.setup(17, GPIO.OUT) # last
 time.sleep(1)
 
 
-GPIO.output(23, 1)
-GPIO.output(25, 1)
-GPIO.output(27, 1)
-GPIO.output(24, 1)
-GPIO.output(22, 1)
-GPIO.output(17, 1)
+GPIO.output(23, 0)
+GPIO.output(25, 0)
+GPIO.output(27, 0)
+GPIO.output(24, 0)
+GPIO.output(22, 0)
+GPIO.output(17, 0)
 
-time.sleep(10)
+time.sleep(1)
 
 
-
-#for x in range(1, 30):
-#	print x
-#	GPIO.setup(x, GPIO.OUT)
-#	GPIO.output(x, 1)
-#	GPIO.output(x, 0)
-#	time.sleep(1)
 
 for x in range(0, 3):
 
   GPIO.output(23, 1)
-  time.sleep(1)
   GPIO.output(25, 1)
-  time.sleep(1)
   GPIO.output(27, 1)
-  time.sleep(1)
   GPIO.output(24, 1)
-  time.sleep(1)
   GPIO.output(22, 1)
-  time.sleep(1)
   GPIO.output(17, 1)
-  time.sleep(1)
 
+
+  time.sleep(2)
   
   
   GPIO.output(23, 0)
-  time.sleep(1)
   GPIO.output(25, 0)
-  time.sleep(1)
   GPIO.output(27, 0)
-  time.sleep(1)
   GPIO.output(24, 0)
-  time.sleep(1)
   GPIO.output(22, 0)
-  time.sleep(1)
   GPIO.output(17, 0)
-  time.sleep(1)
