82c25ee3af73f8dac1f2edcbdecfd76d07ae5ee8
alexander.kosenko <alexander.kosenko@loopme.com>
2016-12-04 01:54:16 +0200

    Sun Dec  4 01:54:16 EET 2016

--- grow.py
+++ grow.py
@@ -2,8 +2,8 @@
 
 from functions import *
 
-grow_day_duration = 16
-grow_night_duration = 8
+grow_day_duration = 2
+grow_night_duration = 1
 chronon = 10
 debug = 1
 init_time = init()
@@ -28,21 +28,28 @@ while True:
 		print "Day!"
 		sqlite_insert_into_log("Day!")		
 		enable_led()
+		print "led was enabled"
+		print hour_of_cycle
 
-	elif hour_of_cycle >= grow_day_duration and hour_of_cycle < grow_day_duration + grow_night_duration:
+	elif hour_of_cycle >= grow_day_duration and hour_of_cycle < (grow_day_duration + grow_night_duration):
 		print "Night!"
 		sqlite_insert_into_log("Night!")
-		disable_led()
+		disable_led()	
+		print "led was disabled"
+		print hour_of_cycle
 
 	else:
 		print "New day!"
+		print hour_of_cycle
 		sqlite_insert_into_log("New day!")
 		diff = 0
 		inc_day_of_grow()
 		init_time = init(reset = 1)
 		enable_pump()
+		print "pump was enabled"
 		time.sleep(2)
-		disable_pump()
+		disable_pump()		
+		print "pump was disabled"
 		#if debug:
 		#break
 
