22adfb78a24a5e16752da6ff341bddc33dd0d7df
alexander.kosenko <alexander.kosenko@loopme.com>
2016-12-04 01:35:04 +0200

    Sun Dec  4 01:35:04 EET 2016

--- grow.py
+++ grow.py
@@ -3,30 +3,20 @@
 from functions import *
 
 grow_day_duration = 16
-grow_night_duratin = 8
-chronon = 1
+grow_night_duration = 8
+chronon = 10
 debug = 1
 init_time = init()
-
-# Initiate GPIO for pigpio
-pi = pigpio.pi()
-# Setup the sensor
-dht22 = DHT22.sensor(pi, 4) # use the actual GPIO pin name
-dht22.trigger()
-
 relay_init()
-
+disable_led()
+disable_cool()
+disable_pump()
+time.sleep(5)
 
 if debug:
 	diff = 0
 
 while True:
-	#if debug:
-	#	print diff
-	#	diff += 1
-	#else:
-	#	diff = int(time.time() - init_time)
-
 
 	temp, hum = readDHT22()
 	sqlite_insert_into_sensors(str(temp), str(hum))
@@ -34,32 +24,26 @@ while True:
 
 	hour_of_cycle =  int(int(time.time() - init_time) / 3600)
 	print "Current hour is:" + str(hour_of_cycle)
-	if hour_of_cycle >= 0 and hour_of_cycle < grow_day_duration:
+	if hour_of_cycle >= 0 and hour_of_cycle < grow_day_duration: # DAY
 		print "Day!"
-		sqlite_insert_into_log("Day!")
-		#enable_led()
+		sqlite_insert_into_log("Day!")		
+		enable_led()
 
-		### ENABLE WATERING ###
-		### DISABLE WATERING ###
-		### ENABLE LED ###
-
-	### ENABLE COOL ###
-
-	elif hour_of_cycle >= grow_day_duration and hour_of_cycle < grow_day_duration + grow_night_duratin:
+	elif hour_of_cycle >= grow_day_duration and hour_of_cycle < grow_day_duration + grow_night_duration:
 		print "Night!"
 		sqlite_insert_into_log("Night!")
 		disable_led()
-		### DISABLE LED ###
+
 	else:
 		print "New day!"
 		sqlite_insert_into_log("New day!")
 		diff = 0
 		inc_day_of_grow()
 		init_time = init(reset = 1)
-		enable_watering()
+		enable_pump()
 		time.sleep(2)
-		disable_watering()
+		disable_pump()
 		#if debug:
-		break
+		#break
 
-	#time.sleep(chronon)
+	time.sleep(chronon)
